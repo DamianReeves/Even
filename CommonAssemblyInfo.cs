c7ce04e58254fe14f265338783db40e072f70d5c
Damian Reeves <damian@mohawkgp.com>
2016-06-07 14:07:52 -0400

    Support packaging

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
 [assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.0-build.1+146.Branch.feature/build.Sha.af869097818a43671999a7b608055895f6521420")]
+[assembly: AssemblyInformationalVersion("0.1.0-build.1+147.Branch.feature/build.Sha.dc208f7ff3810694ee4eee067ad1f9a61e793fef")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
--- build/default.ps1
+++ build/default.ps1
@@ -4,23 +4,18 @@ properties {
     $base_directory = Resolve-Path ..
     $publish_directory = "$base_directory\.build\publish"
     $build_output_root_directory = "$base_directory\.build"
-    $build_directory = "$base_directory\build"
     $src_directory = "$base_directory\src"
     $test_directory = "$base_directory\test"
     $samples_directory = "$base_directory\samples"
-    $output_directory = "$base_directory\.build\output"
-    $packages_directory = "$src_directory\packages"
-    $sln_file = "$base_directory\Even.sln"
+    $artifacts_directory = "$base_directory\.build\artifacts"
     $src_projects = gci $src_directory -Include *.csproj -Recurse
     $test_projects = gci $test_directory -Include *.csproj -Recurse
     $sample_projects = gci $samples_directory -Include *.csproj -Recurse
     $projects = $src_projects + $test_projects + $sample_projects  
     $target_config = "Release"
-    $framework_version = "v4.0"
 
     $assemblyInfoFilePath = "$base_directory\CommonAssemblyInfo.cs"
 
-    $xunit_path = "$base_directory\bin\xunit.runners.1.9.1\tools\xunit.console.clr4.exe"
     $ilMergeModule.ilMergePath = "$base_directory\bin\ilmerge-bin\ILMerge.exe"
     $nuget_dir = "$src_directory\.nuget"
 
@@ -47,36 +42,59 @@ task Test -depends RunUnitTests
 
 task clean-source-projects {    
     foreach ($project in $src_projects) {
-        exec { msbuild /nologo /verbosity:quiet $project /p:Configuration=$target_config /t:Clean }	            
+        $projectDir = $project.DirectoryName
+        $projectName = Split-Path $projectDir -Leaf
+        $projectOutputs = Join-Path $artifacts_directory $projectName 
+        EnsureDirectory ($projectOutputs)
+        exec { msbuild /nologo /verbosity:quiet $project /p:Configuration=$target_config /t:Clean }	                    
     }
 }
 
 task clean-test-projects {    
     foreach ($project in $test_projects) {
+        $projectDir = $project.DirectoryName
+        $projectName = Split-Path $projectDir -Leaf
+        $projectOutputs = Join-Path $artifacts_directory $projectName
+        EnsureDirectory ($projectOutputs)
         exec { msbuild /nologo /verbosity:quiet $project /p:Configuration=$target_config /t:Clean }	            
     }
 }
 
 task clean-sample-projects {
     foreach ($project in $sample_projects) {
+        $projectDir = $project.DirectoryName
+        $projectName = Split-Path $projectDir -Leaf
+        $projectOutputs = Join-Path $artifacts_directory $projectName
+        EnsureDirectory ($projectOutputs)
         exec { msbuild /nologo /verbosity:quiet $project /p:Configuration=$target_config /t:Clean }	            
     }
 }
 
 task compile-source-projects -depends clean-source-projects {
     foreach ($project in $src_projects) {
-	    exec { msbuild /nologo /verbosity:quiet $project /p:Configuration=$target_config /p:TargetFrameworkVersion=v4.5.1 }        
+        $projectDir = $project.DirectoryName
+        $projectName = Split-Path $projectDir -Leaf
+        $projectOutputs = Join-Path $artifacts_directory $projectName
+        $outDir = Join-path $projectOutputs "bin"
+        if (-not ($outdir.EndsWith("\"))) { 
+            $outdir += '\' #MSBuild requires OutDir end with a trailing slash #awesome 
+        }
+
+        if ($outdir.Contains(" ")) { 
+            $outdir="""$($outdir)\""" #read comment from Johannes Rudolph here: http://www.markhneedham.com/blog/2008/08/14/msbuild-use-outputpath-instead-of-outdir/ 
+        } 
+	    exec { msbuild /nologo /verbosity:quiet $project /p:Configuration=$target_config /p:TargetFrameworkVersion=v4.5.1 /p:OutDir=$outdir }        
     }    
 }
 
 task compile-test-projects -depends clean-test-projects {
-    foreach ($project in $test_projects) {
+    foreach ($project in $test_projects) {        
 	    exec { msbuild /nologo /verbosity:quiet $project /p:Configuration=$target_config /p:TargetFrameworkVersion=v4.5.1 }        
     }    
 }
 
 task compile-sample-projects -depends clean-sample-projects {
-    foreach ($project in $sample_projects) {
+    foreach ($project in $sample_projects) {        
 	    exec { msbuild /nologo /verbosity:quiet $project /p:Configuration=$target_config /p:TargetFrameworkVersion=v4.5.1 }        
     }    
 }
@@ -86,42 +104,63 @@ task Compile -depends compile-source-projects, compile-test-projects, compile-sa
 
 task RunUnitTests -depends acquire-testingtools {
 	"Unit Tests"
-	EnsureDirectory $output_directory
     $xunit_path = $script:xunit_path
-    $tests = ls ("{0}/*/bin/$target_config" -f $test_directory) -Include '*.Tests.dll' -Recurse 
     $hasError = $false
-    foreach ($test in $tests) {
-        $testOutput = [System.IO.Path]::ChangeExtension($test, "testresults.xml")        
+    foreach ($testProj in $test_projects) {
+        $projectDir = $testProj.DirectoryName
+        $projectName = Split-Path $projectDir -Leaf
+        $projectOutputs = (Join-Path $artifacts_directory $projectName)
+        $testResultsDirectory =  (Join-Path $projectOutputs "testResults")
+        $testResultsFile = (Join-Path $testResultsDirectory ("{0}-testResults.xml" -f $projectName))        
+        EnsureDirectory($testResultsDirectory)
+        $test = ls ("{0}/bin/$target_config" -f $projectDir) -Include '*.Tests.dll' -Recurse | select -First 1
+                
         $cmdArgs = @("$test")
         $cmdArgs += "-xml"
-        $cmdArgs += '"{0}"' -f $testOutput
-        & $xunit_path $cmdArgs
-        if( $LASTEXITCODE -ne 0) {
-            $hasError = $true
-        }             
-    }    
+        $cmdArgs += '"{0}"' -f $testResultsFile
+        exec {& $xunit_path $cmdArgs}        
+    }	           
 
     if ($hasError) {
         throw "RunUnitTests: Error encountered while executing xunit tests"
     }
 }
 
-task Package -depends Build {
-	move $output_directory $publish_directory
-}
+task Package -depends Build, nuget-pack 
 
 task clean-directories {
     Clean-Item $publish_directory -ea SilentlyContinue
-    Clean-Item $output_directory -ea SilentlyContinue
+    Clean-Item $artifacts_directory -ea SilentlyContinue
     Clean-Item $build_output_root_directory
     EnsureDirectory $build_output_root_directory
 }
 
-task NuGetPack -depends Package {
-    $versionString = Get-Version $assemblyInfoFilePath
-	$version = New-Object Version $versionString
-	$packageVersion = $version.Major.ToString() + "." + $version.Minor.ToString() + "." + $version.Build.ToString() + "-build" + $build_number.ToString().PadLeft(5,'0')
-	gci -r -i *.nuspec "$nuget_dir" |% { .$nuget_dir\nuget.exe pack $_ -basepath $base_directory -o $publish_directory -version $packageVersion }
+task nuget-pack -depends version {
+    $packageVersion = ($script:VersionInfo).PackageVersion
+    Write-Host "Using PackageVersion: $packageVersion"
+    $nuget = Get-NuGet 
+
+    foreach ($project in $src_projects) {
+        $projectDir = $project.DirectoryName
+        $projectName = Split-Path $projectDir -Leaf
+        $projectOutputs = (Join-Path $artifacts_directory $projectName)
+        $nuspecPath = (Join-Path $projectDir ("{0}.nuspec" -f $projectName))        
+        $nugetOutput = (Join-Path $projectOutputs "nuget")
+
+        EnsureDirectory($projectOutputs)
+        EnsureDirectory($nugetOutput)
+        copy $nuspecPath $projectOutputs -Force
+        $nugetArgs = @('pack')
+        $nugetArgs += ('"{0}"' -f $nuspecPath)
+        $nugetArgs += "-basepath"
+        $nugetArgs += $projectOutputs
+        $nugetArgs += "-o"
+        $nugetArgs += $nugetOutput
+        $nugetArgs += "-version"
+        $nugetArgs += "$packageVersion"
+        exec {& $nuget $nugetArgs }
+        #gci -r -i *.nuspec "$nuget" |% { .$nuget_dir\nuget.exe pack $_ -basepath $base_directory -o $publish_directory -version $packageVersion }
+    }	
 }
 
 task acquire-testingtools {
@@ -136,8 +175,8 @@ task acquire-buildtools -precondition {-not $offline} {
 }
 
 task version -depends acquire-buildtools {    
-    $versionInfo = Invoke-GitVersion -UpdateAssemblyInfo -AssemblyInfoPaths $assemblyInfoFilePath -GitVersionExePath $script:GitVersionExe
-    Write-Host "VersionInfo: $versionInfo"    
+    $script:versionInfo = Invoke-GitVersion -UpdateAssemblyInfo -AssemblyInfoPaths $assemblyInfoFilePath -GitVersionExePath $script:GitVersionExe
+    Write-Host ('VersionInfo: {0}' -f $script:versionInfo)    
 }
 
 function EnsureDirectory {
